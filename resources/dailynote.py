c6221d30be4765772b5c6c8c6efac8844fd314fc
YeojinSon7 <130967465+YeojinSon7@users.noreply.github.com>
2023-09-05 18:18:19 +0900

    알림장 등록 api 수정

--- resources/dailynote.py
+++ resources/dailynote.py
@@ -5,7 +5,7 @@ from mysql.connector import Error
 from mysql_connection import get_connection
 from utils import check_password, hash_password
 from email_validator import validate_email,EmailNotValidError 
-from flask_jwt_extended import create_access_token, get_jwt, jwt_required
+from flask_jwt_extended import create_access_token, get_jwt, jwt_required, get_jwt_identity
 from datetime import datetime
 from config import Config
 import boto3
@@ -50,45 +50,48 @@ class DailyNoteDeleteResource(Resource):
         return {'result' : '알림장 기록이 삭제되었습니다'}
 
 
-class DailyNoteAddResource(Resource) :
+class DailyNoteAddResource(Resource) : #선생님이 알림장 등록
 
     @jwt_required()
     def post(self,childId):
+
+        teacherId = get_jwt_identity()
+        data = request.get_json()
          # 사진이 필수인 경우의 코드
-        if 'photoUrl' not in request.files or 'title' not in request.form or 'contents' not in request.form or 'dailyTemperCheck' not in request.form or 'dailyMealCheck' not in request.form or 'dailyNapCheck' not in request.form or 'dailyPooCheck' not in request.form: # 두 줄로 하고싶으면 \ (역슬래시) 이용해라
-            return{'result':'fail','error':'필수항목 확인'},400
-        # 유저가 올린 파일을 변수로 만든다
-        file = request.files['photoUrl']
-        title = request.form['title']
-        contents = request.form['contents']
-        dailyTemperCheck = request.form['dailyTemperCheck']
-        dailyMealCheck = request.form['dailyMealCheck']
-        dailyNapCheck = request.form['dailyNapCheck']
-        dailyPooCheck = request.form['dailyPooCheck']
+        # if 'photoUrl' not in request.files or 'title' not in request.form or 'contents' not in request.form or 'dailyTemperCheck' not in request.form or 'dailyMealCheck' not in request.form or 'dailyNapCheck' not in request.form or 'dailyPooCheck' not in request.form: # 두 줄로 하고싶으면 \ (역슬래시) 이용해라
+        #     return{'result':'fail','error':'필수항목 확인'},400
+        # # 유저가 올린 파일을 변수로 만든다
+        # file = request.files['photoUrl']
+        # title = request.form['title']
+        # contents = request.form['contents']
+        # dailyTemperCheck = request.form['dailyTemperCheck']
+        # dailyMealCheck = request.form['dailyMealCheck']
+        # dailyNapCheck = request.form['dailyNapCheck']
+        # dailyPooCheck = request.form['dailyPooCheck']
 
-        # 2. 사진부터 s3에 저장
-        # 파일명을 유니크하게 만들어준다
-        current_time = datetime.now()
+        # # 2. 사진부터 s3에 저장
+        # # 파일명을 유니크하게 만들어준다
+        # current_time = datetime.now()
 
-        new_filename = current_time.isoformat().replace(':','_').replace('.','_')+'.jpg'
+        # new_filename = current_time.isoformat().replace(':','_').replace('.','_')+'.jpg'
 
-        # 새로운 파일명으로, s3에 파일 업로드
-        try:
-            s3 = boto3.client('s3', #s3에 올릴거다
-                     aws_access_key_id =  Config.AWS_ACCESS_KEY_ID,
-                     aws_secret_access_key = Config.AWS_SECRET_ACCESS_KEY) # 올릴려면 권한이 필요하다 
-        # 그냥 액세스키를 넣어주면 짜면 회사에서 짤린다! 보안때문에!
-            s3.upload_fileobj(file,
-                            Config.S3_BUCKET,
-                            new_filename,
-                            ExtraArgs = {'ACL':'public-read', 'ContentType':'image/jpeg'}) # 퍼블릭으로해야 웹페이지에서도 볼 수 있다? / 우리는 사진을 jpeg 타입으로 할거에요
-        except Exception as e:
-            print(str(e))
-            return{'result':'fail','error':str(e)},500
+        # # 새로운 파일명으로, s3에 파일 업로드
+        # try:
+        #     s3 = boto3.client('s3', #s3에 올릴거다
+        #              aws_access_key_id =  Config.AWS_ACCESS_KEY_ID,
+        #              aws_secret_access_key = Config.AWS_SECRET_ACCESS_KEY) # 올릴려면 권한이 필요하다 
+        # # 그냥 액세스키를 넣어주면 짜면 회사에서 짤린다! 보안때문에!
+        #     s3.upload_fileobj(file,
+        #                     Config.S3_BUCKET,
+        #                     new_filename,
+        #                     ExtraArgs = {'ACL':'public-read', 'ContentType':'image/jpeg'}) # 퍼블릭으로해야 웹페이지에서도 볼 수 있다? / 우리는 사진을 jpeg 타입으로 할거에요
+        # except Exception as e:
+        #     print(str(e))
+        #     return{'result':'fail','error':str(e)},500
         
-        # 3. 위에서 저장한 사진의 URL 주소와 내용을 DB에 저장해야 한다!
-        # URL 주소는 = 버킷명.S3주소/우리가 만든 파일명
-        file_url = Config.S3_BASE_URL+ new_filename # 이미지 url 부분은 회사마다 다르다
+        # # 3. 위에서 저장한 사진의 URL 주소와 내용을 DB에 저장해야 한다!
+        # # URL 주소는 = 버킷명.S3주소/우리가 만든 파일명
+        # file_url = Config.S3_BASE_URL+ new_filename # 이미지 url 부분은 회사마다 다르다
         try:
             # 2-1. 데이터베이스를 연결한다.
             connection = get_connection()
@@ -96,12 +99,12 @@ class DailyNoteAddResource(Resource) :
             # 2-2. 쿼리문 만든다
             ###### 중요! 컬럼과 매칭되는 데이터만 %s로 바꿔준다.
             query = '''insert into dailyNote
-                    (childId,title,contents,photoUrl,dailyTemperCheck,dailyMealCheck,dailyNapCheck,dailyPooCheck)
+                    (teacherId,childId,title,contents,dailyTemperCheck,dailyMealCheck,dailyNapCheck,dailyPooCheck)
                     values
-                    (%s,%s,%s,%s,%s,%s,%s,%s);'''
+                    (%s,%s,%s,%s,%s,%s,%s,%s,%s);'''
             #2-3. 쿼리에 매칭되는 변수 처리! 중요! 튜플로 처리해준다!(튜프은 데이터변경이 안되니까?)
             # 위의 %s부분을 만들어주는거다
-            record = (childId,title,contents,file_url,dailyTemperCheck,dailyMealCheck,dailyNapCheck,dailyPooCheck)
+            record = (teacherId,childId,data['title'],data['contents'],data['dailyTemperCheck'],data['dailyMealCheck'],data['dailyNapCheck'],data['dailyPooCheck'])
             #2-4 커서를 가져온다
             cursor = connection.cursor(prepared=True)
             #2-5 쿼리문을,커서로 실행한다.
