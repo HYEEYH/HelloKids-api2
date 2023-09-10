003cf2a4ee770e67d99656cdd892e7cc06bd8697
YeojinSon7 <130967465+YeojinSon7@users.noreply.github.com>
2023-09-10 13:38:11 +0900

    출석부- 학부모의 원아 출석부 목록 조회 API 수정

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ api.add_resource(AttendanceChildrenListResource,'/attendance/class/children') # 
 api.add_resource(AttendanceAddResource,'/attendance/add/<int:childId>') # 출석 체크 생성
 api.add_resource(AttendanceClassListResource,'/attendance/teacher/class') # 선생님이 속한 반의 출석부 목록 조회
 api.add_resource(AttendanceEditResource,'/attendance/edit/<int:id>') # 출석 체크 수정
-api.add_resource(AttendanceChildListResource,'/attendance/children/<int:id>') # 선생님이 속한 반의 출석부 목록 조회
+api.add_resource(AttendanceChildListResource,'/attendance/parents/children') # 선생님이 속한 반의 출석부 목록 조회
 
 # 번역
 api.add_resource(TranslateResource, '/translate')
